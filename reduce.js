3182c384e60d6aaeb86259097c7b814ac7e4f8a0
Aphenphos <MaxwillWinters@gmail.com>
2022-09-20 13:49:14 -0400

    map 1

--- reduce.js
+++ reduce.js
@@ -2,7 +2,15 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce(function(acc, item) {
+    if (!item[brands.brand]) {
+      return acc;
+    }
+    acc.push(item[brands.brand]);
+    return acc;
+  }, brands.brand);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
